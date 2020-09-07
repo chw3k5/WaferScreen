412e8d68e1226dc6767923daee5cb3527597fe19
cryostatlazarus <cryostatlazarus@gmail.com>
2020-09-07 16:08:06 -0600

    tried to get pyserial to work

--- waferscreen/inst_control/srs_sim928.py
+++ waferscreen/inst_control/srs_sim928.py
@@ -1,19 +1,44 @@
 import visa
 import time
+import serial
 
 
 class SRS_SIM928(): 
     """ Stanford Research Systems SIM928 Isolated Voltage Source """
-    def __init__( self, address="GPIB0::16::INSTR", port = 1):
-        self.ResourceManager = visa.ResourceManager()
-        self.ctrl = self.ResourceManager.open_resource( "%s" % address, write_termination='\n' )
-        self.ctrl.timeout = 1000
-        self.ctrl.port = int(round(port))
-        self.ctrl.write("FLOQ")
-        self.ctrl.write('SNDT ' + str(self.ctrl.port) + ', "*IDN?"')
+    def __init__(self, com_num=None, address="GPIB0::16::INSTR", port=1):
+        self.port = str(port)
+        if com_num is None:
+            self.ResourceManager = visa.ResourceManager()
+            self.ctrl = self.ResourceManager.open_resource("%s" % address, write_termination='\n')
+            self.ctrl.timeout = 1000
+            self.port = int(round(port))
+            self.is_gpib = True
+        else:
+            self.ctrl = serial.Serial()
+            self.ctrl.baudrate = '115200'
+            self.ctrl.port = F"COM{com_num}"
+            self.ctrl.open()
+            self.is_open = self.ctrl.is_open
+            self.is_gpib = False
+        self.port = str(port)
+        self.say_hello()
+
+    def write(self, write_str):
+        if self.is_gpib:
+            self.ctrl.write(write_str)
+        else:
+            self.ctrl.write(write_str.encode("utf-8"))
+
+    def say_hello(self):
+        self.write("FLOQ")
+        self.write('SNDT ' + self.port + ', "*IDN?"')
         time.sleep(0.1)
-        self.ctrl.device_id = self.ctrl.query('GETN? ' + str(self.ctrl.port) + ',100')[5:].rstrip()
-        print("Connected to : " + self.ctrl.device_id)
+        if self.is_gpib:
+            self.device_id = self.ctrl.query('GETN? ' + str(self.ctrl.port) + ',100')[5:].rstrip()
+        else:
+            self.write('GETN? ' + self.port + ',100')
+            self.device_id = str(self.ctrl.read_all())
+        print("Connected to : " + self.device_id)
         
     def setvolt(self, volts = 0):
         #print('SNDT ' + str(self.ctrl.port) + ', "VOLT ' + str(volts) + '"')
@@ -36,3 +61,7 @@ class SRS_SIM928():
     def close(self):
         self.ctrl.close()
         print("Voltage source control closed")
+
+
+if __name__ == "__main__":
+    vc = SRS_SIM928(com_num=2)
