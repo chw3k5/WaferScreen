6f967d550f3b8595319c997f188e2a7e79134cbc
Caleb Wheeler <chw3k5@gmail.com>
2021-03-02 12:27:01 -0700

    Finished IV sweep codes

--- waferscreen/inst_control/iv_sweep.py
+++ waferscreen/inst_control/iv_sweep.py
@@ -4,6 +4,7 @@ import datetime
 import numpy as np
 from ref import working_dir
 from waferscreen.inst_control.keithley2450 import Keithley2450
+import matplotlib.pyplot as plt
 
 
 location = 'nist'
@@ -30,6 +31,8 @@ class IVSweep:
         self.verbose = verbose
 
         self.source = Keithley2450(connection_type=self.connection_type, verbose=self.verbose)
+        self.source.startup()
+        self.source.set_source_type(v_range=b"2", i_range=b"100e-6")
 
         self.output_file_name = None
         self.start_ua = None
@@ -40,23 +43,16 @@ class IVSweep:
         self.sweeps_array_v = None
         self.meas_utc = None
 
-    def sweep(self, start_ua, end_ua, num_points=100):
+    def sweep(self, start_ua, end_ua, num_points=101, v_gain=1.0):
         self.start_ua = start_ua
         self.end_ua = end_ua
         if self.start_ua > self.end_ua:
             self.start_ua, self.end_ua = self.end_ua, self.start_ua
         self.num_points = num_points
-        self.step_ua = (end_ua - start_ua) / (num_points - 1)
-        self.sweep_array_ua = np.arange(self.start_ua, self.end_ua + (self.step_ua / 2.0), self.step_ua)
-        self.sweeps_array_v = np.zeros(shape=len(self.sweep_array_ua))
-        if True:  # with self.source:
-            # Here were start send commands to the Keithley 2450, or whatever source meter you have
-            self.source.__enter__()
-            self.source.init_junction_sweep()
-            for current_index, current_ua in list(enumerate(self.sweep_array_ua)):
-                self.source.set_current(current_ua * 1.0E-6)
-                self.sweeps_array_v[current_index] = self.source.get_volt()
 
+        output_data = self.source.sweep(start_curr=start_ua * 1.0e-6, stop_curr=end_ua * 1.0e-6, num_points=num_points, delay_s=0.1)
+        self.sweep_array_ua = np.array([1.0e6 * a_tup[1] for a_tup in output_data])
+        self.sweeps_array_v = np.array([a_tup[2] for a_tup in output_data]) / v_gain
         self.meas_utc = datetime.datetime.utcnow()
             
     def write(self):
@@ -66,9 +62,104 @@ class IVSweep:
             f.write("current_ua,voltage_v\n")
             for current_ua, voltage_v in zip(self.sweep_array_ua, self.sweeps_array_v):
                 f.write(F"{current_ua},{voltage_v}\n")
-        
+
+    def close(self):
+        self.source.shutdown()
+
+    def plot(self):
+        a_array = self.sweep_array_ua * 1.0e-6
+        mv_array = self.sweeps_array_v * 1.0e3
+        res, v_offset = np.polyfit(a_array, self.sweeps_array_v, deg=1)
+        fit_mv_array = ((self.sweep_array_ua * 1.0e-6 * res) + v_offset) * 1.0e3
+        plt.plot(fit_mv_array, self.sweep_array_ua, ls='solid', color="firebrick", linewidth=5)
+        plt.plot(mv_array, self.sweep_array_ua, ls='solid', linewidth=1, color='black',
+                 marker="o", alpha=0.5, markerfacecolor="dodgerblue")
+
+        plt.ylabel("Current (uA)")
+        plt.xlabel("Voltage (mV)")
+        plt.title(F"Resistance: {'%2.3f'% res} Ohms")
+        plt.show(block=True)
+
+
+class VVSweep:
+    def __init__(self):
+        self.sweeps_array_mv = None
+        self.sweeps_array_uv = None
+
+        self.source = Keithley2450(connection_type='lan', verbose=True)
+        self.source.startup()
+        self.source.set_source_type(source_type='voltage', sense_type='voltage', v_range=b'2')
+
+    def sweep(self, start_uv=-10000, end_uv=10000, num_points=101):
+        output_data = self.source.sweep(start_curr=start_uv * 1.0e-6, stop_curr=end_uv * 1.0e-6, num_points=num_points,
+                                        delay_s=0.1)
+        self.sweeps_array_uv = np.array([1.0e6 * a_tup[1] for a_tup in output_data])
+        self.sweeps_array_mv = np.array([1.0e3 * a_tup[2] for a_tup in output_data])
+
+    def plot(self):
+        xv_array = self.sweeps_array_uv * 1.0e-6
+        yv_array = self.sweeps_array_mv * 1.0e-3
+        gain, v_offset = np.polyfit(xv_array, yv_array, deg=1)
+        # plt.plot(self.sweeps_array_uv, (self.sweeps_array_uv * gain) + (v_offset * 1.0e3), ls='solid',
+        #          color="firebrick", linewidth=5)
+        plt.plot(self.sweeps_array_uv, self.sweeps_array_mv, ls='solid', linewidth=1, color='black',
+                 marker="o", alpha=0.5, markerfacecolor="dodgerblue")
+
+        plt.ylabel("Current (mV)")
+        plt.xlabel("Voltage (uV)")
+        plt.title(F"PreAmp Gain: {'%2.3f'% gain} V/V")
+        plt.show(block=True)
+
+    def close(self):
+        self.source.shutdown()
+
+
+def sweeper(start_ua, end_ua, num_points, wafer, wafer_coord, structure_name, plot=True, verbose=True, close=True):
+    iv = IVSweep(wafer=wafer, wafer_coord=wafer_coord, structure_name=structure_name, connection_type='lan',
+                 verbose=verbose)
+    iv.sweep(start_ua=start_ua, end_ua=end_ua, num_points=num_points, v_gain=96.0)
+    if close:
+        iv.close()
+    iv.write()
+    if plot:
+        iv.plot()
+    return iv
+
+
+def warm_squid_sweep(wafer, wafer_coord, structure_name=None):
+    if structure_name is None:
+        structure_name = "warm sweep"
+    return sweeper(start_ua=-20, end_ua=20, num_points=100, wafer=wafer, wafer_coord=wafer_coord,
+                   structure_name=structure_name, plot=True, verbose=True, close=True)
+
+
+def is_alive_squid_sleep(wafer, wafer_coord, structure_name=None):
+    if structure_name is None:
+        structure_name = "warm is_alive"
+    return sweeper(start_ua=-1, end_ua=1, num_points=10, wafer=wafer, wafer_coord=wafer_coord,
+                   structure_name=structure_name, plot=True, verbose=True, close=True)
+
+
+def test_preamp_gain():
+    vv = VVSweep()
+    vv.sweep()
+    vv.plot()
+    vv.close()
+    return vv
+
 
 if __name__ == "__main__":
-    iv = IVSweep(wafer=11, wafer_coord=(0, 0), structure_name='warm res 130ohms', connection_type='lan', verbose=True)
-    iv.sweep(start_ua=0, end_ua=100, num_points=200)
-    iv.write()
+    preamp_gain_test = False
+    is_alive_test = True
+    warm_squid_test = False
+    wafer = 'null'
+    wafer_coord = (0, 0)
+    structure_name = 'warm res 130ohms'
+
+    if preamp_gain_test:
+        vv = test_preamp_gain()
+    if is_alive_test:
+        iv = is_alive_squid_sleep(wafer=wafer, wafer_coord=wafer_coord, structure_name=structure_name)
+    if warm_squid_test:
+        iv = warm_squid_sweep(wafer=wafer, wafer_coord=wafer_coord, structure_name=structure_name)
+
--- waferscreen/inst_control/keithley2450.py
+++ waferscreen/inst_control/keithley2450.py
@@ -3,6 +3,7 @@ import sys
 import time
 import socket
 import serial
+import numpy as np
 
 """
 This is for the Keithley 2450 source meter.
@@ -27,18 +28,32 @@ def read_serial(serial_device):
     return byte_string
 
 
-def read_lan(lan_device, termination=b'\n', buffer=1000):
+def read_single_lan(lan_device, termination=b'\n', buffer=10000):
     all_data = b""
     while all_data == b"" or all_data[-1:] != termination:
         all_data += lan_device.recv(buffer)
     return all_data[:-1]
 
 
+def read_all_lan(lan_device):
+    return lan_device.recv(1000000)
+
+
 def number_format(number):
-    if sys.version_info.major == 3:
-        formatted = bytes(str('%1.6e' % number), "UTF-8")
+    if isinstance(number, int):
+        formatted_str = str('%i' % number)
+    elif isinstance(number, float):
+        sci_notation = str('%e' % number)
+        float_part, power_part = sci_notation.split("e")
+        power_int = int(power_part) - 3
+        float_part_int = int(float(float_part) * 1000.0)
+        if float_part_int == 0:
+            formatted_str = '0'
+        else:
+            formatted_str = str(float_part_int) + "e" + str(power_int)
     else:
-        formatted = str("%1.6e" % number)
+        formatted_str = number
+    formatted = bytes(formatted_str, "UTF-8")
     return formatted
 
 
@@ -56,6 +71,10 @@ class Keithley2450:
         if self.source_mode not in {"current", 'voltage'}:
             raise TypeError(F"source_mode type: {self.source_mode}, is not recognized.")
         self.device = None
+        self.keithley_source_mode = None
+        self.keithley_sense_mode = None
+        self.source_range = None
+        self.sense_range = None
         self.timeout = 2
         self.verbose = verbose
         if self.connection_type == 'lan':
@@ -87,7 +106,7 @@ class Keithley2450:
             
     def read(self):
         if self.connection_type == 'lan':
-            return read_lan(lan_device=self.device, buffer=self.buffer)
+            return read_single_lan(lan_device=self.device)
         elif self.connection_type == 'serial':
             return read_serial(serial_device=self.device)
         
@@ -110,6 +129,42 @@ class Keithley2450:
         if self.verbose:
             print("The output for the Keithley 2450 has been set to OFF")
 
+    def reset(self):
+        self.write(write_str=b"*RST\n")
+
+    def set_source_type(self, source_type=None, sense_type=None, v_range=b'2', i_range=b'100e-3'):
+        if source_type is None:
+            source_type = self.source_mode
+        self.source_mode = source_type
+        if self.source_mode == "current":
+            self.keithley_source_mode = b"CURR"
+            self.source_range = i_range
+            if sense_type is None or sense_type != "current":
+                self.keithley_sense_mode = b"VOLT"
+                self.sense_range = v_range
+            else:
+                self.keithley_sense_mode = b"CURR"
+                self.sense_range = i_range
+        elif self.source_mode == "voltage":
+            self.keithley_source_mode = b"VOLT"
+            self.source_range = v_range
+            if sense_type is None or sense_type != "voltage":
+                self.keithley_sense_mode = b"CURR"
+                self.sense_range = i_range
+            else:
+                self.keithley_sense_mode = b"VOLT"
+                self.sense_range = v_range
+        else:
+            raise TypeError(F"source_mode type: {self.source_mode}, is not recognized.")
+
+        self.write(write_str=b"SOUR:FUNC " + self.keithley_source_mode + b"\n")
+        self.write(write_str=b"SOUR:" + self.keithley_source_mode + b":RANG " + self.source_range + b"\n")
+        self.write(write_str=b"SENS:FUNC \"" + self.keithley_sense_mode + b"\"\n")
+        if self.keithley_source_mode != self.keithley_sense_mode:
+            self.write(write_str=b"SENS:" + self.keithley_sense_mode + b":RANG " + self.sense_range + b"\n")
+        # activated the 4 wire measurements
+        self.write(write_str=b"SENS:" + self.keithley_sense_mode + b":RSEN ON\n")
+
     def get_volt(self):
         self.write(write_str=b'MEAS:VOLT?\n')
         return_str = self.read()
@@ -119,7 +174,6 @@ class Keithley2450:
         return voltage
 
     def set_volt(self, voltage):
-        self.write(write_str=b"SENS:FUNC VOLT\n")
         write_str = b":SOUR:VOLT " + number_format(voltage) + b"\n"
         self.write(write_str=write_str)
         if self.verbose:
@@ -132,8 +186,7 @@ class Keithley2450:
             print(current, "is the read current from the Keithley 2450")
 
     def set_current(self, current_amps):
-        self.write(write_str=b"SOUR:FUNC CURR\n")
-        write_str = b":SOUR:CURR " + number_format(current_amps) + b"\n"
+        write_str = b"SOUR:CURR " + number_format(current_amps) + b"\n"
         self.write(write_str=write_str)
         if self.verbose:
             print("Keithley 2450 was set to a current of", current_amps, "Amps")
@@ -146,13 +199,51 @@ class Keithley2450:
         keithley2450.output_off()
         keithley2450.close()
 
-    def init_sweep(self):
-        self.write(write_str=b"*RST")
-        self.write(write_str=b'''SENS: FUNC "VOLT"''')
-        self.write(write_str=b":SENS:RES:RANG:AUTO ON")
+    def init_sweep(self, v_limit=b"5.0", i_limit=b"0.100"):
+        self.write(write_str=b"SOUR: " + self.keithley_source_mode + b":VLIM " + number_format(v_limit) + b"\n")
+        self.write(write_str=b"SOUR: " + self.keithley_source_mode + b":ILIM " + i_limit + b"\n")
+
+    def sweep(self, start_curr=-0.001, stop_curr=0.001, num_points=21, delay_s=0.1):
+        """
+        :SOURce[1]:SWEep:<function>:LINear:STEP <start>, <stop>, <steps>, <delay>, <count>,
+        <rangeType>, <failAbort>, <dual>, "<bufferName>"
+        SOUR:SWE:CURR:LIN:STEP -1.05, 1.05, .25, 10e-3, 1, FIXED
+        """
+        step_cur = (stop_curr - start_curr) / (num_points - 1.0)
+        sweep_base_str = b"SOUR:SWE:" + self.keithley_source_mode + b":LIN "
+        start_curr_str = number_format(start_curr) + b", "
+        stop_curr_str = number_format(stop_curr) + b", "
+        num_points_str = number_format(num_points) + b", "
+        delay_s_str = number_format(delay_s) + b", "
+        loop_count = number_format(1) + b", "
+        range_type = b"FIXED, "
+        fail_abort = b"OFF, "
+        dual = b"ON, "
+        buffer_name = b'''\"defbuffer1\"'''
+        # calculations
+        sweep_array_a = np.arange(start_curr, stop_curr + (step_cur / 2.0), step_cur)
+        sweep_str = sweep_base_str + start_curr_str + stop_curr_str + num_points_str + delay_s_str + loop_count
+        sweep_str += range_type + fail_abort + dual + buffer_name + b"\n"
+        self.write(write_str=sweep_str)
+        self.write(write_str=b"INIT\n")
+        if self.verbose:
+            print(F"Sweeping:\n   start_curr:{'%1.3f' % (1000 * start_curr)}mA, stop_current:{'%1.3f' % (1000 * stop_curr)}mA, num_points{num_points}")
+        self.write(write_str=b"*WAI\n")
+        get_sweep_str = b"TRAC:DATA? 1, " + bytes(str(len(sweep_array_a)), "UTF-8") + b", " + buffer_name + b", "
+        get_sweep_str += b"SOUR, READ\n"
+        self.write(write_str=get_sweep_str)
+        split_binary_data = self.read().strip().split(b',')
+        meas_data = np.array([float(v_point) for v_point in split_binary_data])
+        output_data = []
+        for set_point_index, set_point in list(enumerate(sweep_array_a)):
+            meas_index = 2 * set_point_index
+            meas_a = meas_data[meas_index]
+            meas_v = meas_data[meas_index + 1]
+            output_data.append((set_point, meas_a, meas_v))
+        return output_data
 
     def get_range_keithley2450(self):
-        write_str = b":CURR:RANG?\n"
+        write_str = b":" + self.keithley_source_mode + b":RANG?\n"
         self.write(write_str=write_str)
         the_range = self.read()
         if self.verbose:
@@ -160,18 +251,19 @@ class Keithley2450:
         return the_range
 
     def zero(self):
-        self.set_volt(voltage=0.0)
+        if self.source_mode == "current":
+            self.set_current(current_amps=0.0)
+        else:
+            self.set_volt(voltage=0.0)
 
     def startup(self):
         self.open()
-        self.zero()
-        self.output_on()
+        self.reset()
     
     def __enter__(self):
         self.startup()
 
     def shutdown(self):
-        self.zero()
         self.output_off()
         self.close()
 
@@ -180,8 +272,9 @@ class Keithley2450:
 
 
 if __name__ == "__main__":
-    keithley2450 = Keithley2450(connection_type='lan', verbose=True)
+    keithley2450 = Keithley2450(connection_type='lan', source_mode="current", verbose=True)
     keithley2450.startup()
-    keithley2450.init_sweep()
+    keithley2450.set_source_type(v_range=b"2e-2", i_range=b"100e-6")
+    sweep_data = keithley2450.sweep()
     keithley2450.shutdown()
 
