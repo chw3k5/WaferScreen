24abe7c85994b5dbe3cceb7244075a7508e85f45
Caleb Wheeler <chw3k5@gmail.com>
2021-04-28 20:56:40 -0500

    metadata to undated processing_metadata_to_remove_from_seeds to in clude 'utc', 'end_time', "start_time", 'fmin_ghz', "fmax_ghz", 'path'

--- waferscreen/analyze/res_pipeline_config.py
+++ waferscreen/analyze/res_pipeline_config.py
@@ -5,4 +5,5 @@ resonator_spacing_threshold_hz = 1.0e5
 
 processing_metadata_to_remove_from_seeds = {"baseline_removed", "baseline_technique", 'fitter_pad_factor',
                                             'group_delay_found_s', 'group_delay_removed', 'group_delay_removed_on',
-                                            'peak_threshold_db', 'smoothing_scale_ghz', 'window_pad_factor'}
+                                            'peak_threshold_db', 'smoothing_scale_ghz', 'window_pad_factor',
+                                            'utc', 'end_time', "start_time", 'fmin_ghz', "fmax_ghz", 'path'}
