c3f0302644b10e2fe83e156236e6de3e262f12fe
chw3k5 <chw3k5@gmail.com>
2021-05-03 18:55:28 -0500

    updates to the setup.py and requirement.txt to make a distribution for Brad.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
 
 setup(name='WaferScreen',
-      version='1.0.3',
+      version='1.0.4',
       description='Testing and database infrastructure for the NIST-SO wafer production',
       author='Caleb Wheeler, Zach Whipps, Johannes Hubmayr, Jordan Wheeler, Jake Connors',
       author_email='chw3k5@gmail.com',
-      packages=['waferscreen'],
+      packages=['waferscreen', "gluerobot", "submm_python_routines"],
+      url="https://github.com/chw3k5/WaferScreen",
       requires=['numpy', 'matplotlib', 'scipy', "pyvisa", "LabJackPython", "PySerial", "numba"]
       )
